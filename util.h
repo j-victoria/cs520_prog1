32813389d8225edbad85131383839f4c8dd73899
Jacquelyn Victoria <jvictor5@cs.binghamton.edu>
2016-12-10 11:37:37 -0500

    start of p2, testing IQ, ROB & brach handling

---
+++ util.h
@@ -0,0 +1,39 @@
+//structs
+//#include "inst.h"
+#include "instructions.h"
+using namespace std;
+#include <string>
+
+typedef struct instruction_t {
+  int dest, src1_v, src1_a, src2_v, src2_a, lit, res, index, dc, rob_loc, dest_ar, iq_loc, latch_loc;
+  bool src1, src2, valid, in_iq;
+  enum instructions_t type;
+  string name;
+}Instruction;
+
+typedef struct reg {
+  int value;
+  bool valid;
+  Instruction * producer;
+  vector<Instruction *> consumer1;
+  vector<Instruction *> consumer2;
+}Register;
+
+typedef struct iq_entry {
+  Instruction * inst;
+  bool valid;
+}iqe;
+
+typedef struct rat_entry {
+  Register * reg;
+  Instruction * producer;
+  vector <Instruction> consumers;
+}rate;
+
+typedef struct rob_entry {
+  Instruction * inst;
+  bool finished;
+  bool valid;
+  int renaming;
+}robe;
+
